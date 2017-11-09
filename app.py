931712d423ae7f3995132f0ffd73d53f07405983
Katricia Herring <katricia.herring@gmail.com>
2017-11-09 17:31:26 -0500

    update

--- app.py
+++ app.py
@@ -97,15 +97,14 @@ def query():
         _search = request.form['search']
         _year=request.form['year']
         # validate the received values
-        if _year and _search:
-            conn = mysql.connect()
-            cursor = conn.cursor()
-            query_string="SELECT l.name FROM laws l, publications p  WHERE l.pub_id=p.pub_id and l.name like '%" + _search + "%' and EXTRACT(YEAR FROM p.pub_date) ="+_year+""
-            cursor.execute(query_string)
-            if cursor.fetchall(): #results exit
-                kwargs['data'] = cursor.fetchall()
-            else:
-                kwargs['data'] = "No results found matching inputted criteria"
+        conn = mysql.connect()
+        cursor = conn.cursor()
+        query_string="SELECT l.name FROM laws l, publications p  WHERE l.pub_id=p.pub_id and EXTRACT(YEAR FROM p.pub_date) ="+_year+""
+        cursor.execute(query_string)
+        if cursor.fetchall(): #results exit
+            kwargs['data'] = cursor.fetchall()
+        else:
+            kwargs['data'] = "No results found matching inputted criteria"
 
     except Exception as e:
         kwargs['message'] = "Error %s: %s" % (e[0], e[1])
