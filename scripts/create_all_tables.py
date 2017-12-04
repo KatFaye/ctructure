f9f6421b32daf6f7fd07e463aeacb4d1d3a5cde1
Samson-Dai <dscsamson@gmail.com>
2017-12-04 10:25:25 -0500

    debug

--- scripts/create_all_tables.py
+++ scripts/create_all_tables.py
@@ -137,7 +137,8 @@ tables['users'] = (
 try:
     cnx = mc.connect(**config)
     cursor = cnx.cursor()
-    exit()
+    print("We're here")
+    #exit()
 except mc.Error as err:
     if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
         print("Err: Access Denied (Verify user and password)")
