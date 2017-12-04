29fdbf71573dbd3d969e6eb825381a3f943bad87
Samson-Dai <dscsamson@gmail.com>
2017-12-04 10:00:28 -0500

    complete filter

--- scripts/advanced_search.py
+++ scripts/advanced_search.py
@@ -140,11 +140,31 @@ def build_query(query_str, agency_in, content_in, pub_year_in):
     user_filter = None
   # 2. All filters are selected. 
   elif agency and content_type and pub_year:
-    user_filter = And([Term("agency_tag", get_unicode(agency)), \
-                       Term("content_type_tag", get_unicode(content_type)), \
-                       Term("pub_year", pub_year))
+    user_filter = And([Term("agency_tag", get_unicode(agency)),
+                       Term("content_type_tag", get_unicode(content_type)),
+                       Term("pub_year", pub_year)])
+  # 3. agency and content_type
+  elif agency and content_type:
+    user_filter = And([Term("agency_tag", get_unicode(agency)),
+                       Term("content_type_tag", get_unicode(content_type))])
+  # 4. agency and publication
+  elif agency and publication:
+    user_filter = And([Term("agency_tag", get_unicode(agency)),
+                       Term("pub_year", pub_year)])
+  # 5. agency and publication
+  elif content_type and agency:
+    user_filter = And([Term("content_type_tag", get_unicode(content_type)),
+                       Term("pub_year", pub_year)])
+  # 6. Agency alone
+  elif agency:
+    user_filter = And([Term("agency_tag", get_unicode(agency))])
+  # 7. Content rype alone
+  elif content_type:
+      user_filter = And([ Term("content_type_tag", get_unicode(content_type))])
+  # 8. Publication alone
+  elif publication:
+      user_filter = And([Term("pub_year", pub_year)])
 
-  # 3. Agency alone
   
   
   
