390c8c5aac4bd5f7ada7fc864fc0b51bb9536d3b
YunLovesCode <31970796+YunLovesCode@users.noreply.github.com>
2017-12-04 22:46:08 -0500

    modify references

--- scripts/references.py
+++ scripts/references.py
@@ -16,9 +16,18 @@ for ref in os.listdir('demo_refs'):
           #print(current_line)
 
           if (i == 2):
+              new_obj = FixLawName(current_line)
+              copy = new_obj.copy_list()
+              print(copy)
 
-              lawName = current_line
-              print(lawName)
+              for k in range(len(copy)):
+                  for j in range(len(copy[k]) - 4):
+                      token = copy[k][j]
+                      if new_obj.is_law_name(copy, k, j):
+                          if copy[k][j + 1][0].isdigit():
+                              parent_law_num = copy[k][j + 1]
+                              parent_law_date = copy[k][j + 3]
+                              print(parent_law_num, parent_law_date)
           elif(i==3):
               new_obj = FixLawName(current_line)
               copy = new_obj.copy_list()
@@ -32,19 +41,5 @@ for ref in os.listdir('demo_refs'):
                               law_num = copy[k][j + 1]
                               law_date = copy[k][j + 3]
                               print(law_num, law_date)
-              '''publicationDateMatch = re.findall(r'\d{2}/\d{2}/\d{4}', current_line)
-              lawNumMatch = re.findall(r'\d{2}/\d{4}', current_line)
-              if publicationDateMatch is not None:
-                  articleNums = []
-                  for s in current_line.split():
-                    j=0
-                    if s=="Articles":
-                      articleNums.append(s)
-                      j=j+1
-                    #if s.isdigit():
-                          #print(s)
-                  print(publicationDateMatch)
-              if lawNumMatch is not None:
-                  print(lawNumMatch)'''
+
           i=i+1
-          print("\n")
