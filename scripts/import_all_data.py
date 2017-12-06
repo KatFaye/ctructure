3387eee32ec8255c517c55eaa571aeb987e7ca27
Anselme Mucunguzi <mucunguzia@gmail.com>
2017-12-05 23:54:15 -0500

    debugging

--- scripts/import_all_data.py
+++ scripts/import_all_data.py
@@ -99,8 +99,7 @@ unique_publications = {}
 
 # Extract publication table attributes from publication names
 for publication in publication_names:
-    if publication:
-      print("Publication: ", publication)
+    if publication and type(publication) != type(None):
       var_num, pub_date = get_pub_attrs(publication)
       # Make sure it's "of" not "0f" [zero instead of o]
       split_pub = publication.split()
