fae844ce8a1148e9e9521a7c951ce9c8e20b11be
Samson-Dai <dscsamson@gmail.com>
2017-12-06 03:40:52 -0500

    debug

--- app.py
+++ app.py
@@ -2,8 +2,6 @@ from flask import Flask, render_template, json, request, redirect, flash, sessio
 from flaskext.mysql import MySQL
 from base import base_page
 from os import urandom
-from scripts import advanced_search
-from advanced_search import build_filters, get_results
 
 app = Flask(__name__)
 app.register_blueprint(base_page)
