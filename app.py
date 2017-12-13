f23d3dfd6be4715443e34b95fb13c8d4f694cdd2
Samson-Dai <dscsamson@gmail.com>
2017-12-12 22:35:24 -0500

    debug

--- app.py
+++ app.py
@@ -164,11 +164,11 @@ def query():
         query_results = get_results(_agency, _content_type, _year,_search)
         print("I'M HERE!!!!!!!!!!!!!!!!!!!!!\n")
         print(query_results)
-        print(type(query_results[0]))
+        print(type(query_results))
         
         #query_string="SELECT l.name FROM laws l, publications p  WHERE l.pub_id=p.pub_id and l.name like '%" + _search + "%' and EXTRACT(YEAR FROM p.pub_date) ="+_year+""
-        query_string="SELECT l.name FROM laws l  WHERE  l.name like '%" + _search + "%'"
-        cursor.execute(query_string)
+        #query_string="SELECT l.name FROM laws l  WHERE  l.name like '%" + _search + "%'"
+        #cursor.execute(query_string)
         
         #kwargs['data'] = cursor.fetchall()
         kwargs['message'] = query_results
