ccef0c254e98a26f25fff64d6e5baa19b2691263
Anselme Mucunguzi <mucunguzia@gmail.com>
2017-12-13 23:10:47 -0500

    debug

--- app.py
+++ app.py
@@ -167,7 +167,7 @@ def query():
 
         query_results = get_results(_agency, _content_type, _year, _search)
         print("I'M HERE!!!!!!!!!!!!!!!!!!!!!\n")
-        print(type(query_results[0]["law_name"]))
+        print(query_results[0]["law_name"])
 
         #query_string="SELECT l.name FROM laws l, publications p  WHERE l.pub_id=p.pub_id and l.name like '%" + _search + "%' and EXTRACT(YEAR FROM p.pub_date) ="+_year+""
         #query_string="SELECT l.name FROM laws l  WHERE  l.name like '%" + _search + "%'"
