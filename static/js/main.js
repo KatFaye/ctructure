ae166bb42031dc9a53316fe503627e51bddd865a
Samson-Dai <dscsamson@gmail.com>
2017-12-14 16:26:12 -0500

    debug

--- static/js/main.js
+++ static/js/main.js
@@ -372,7 +372,7 @@ $( document ).ready(function(){
                             'Cited Laws: '+ 
                         '</th></tr>'+
                         '<tr><td>'+
-                        	cited+
+                        	cited_law+
                         '</td></tr>'+
                     '</table>'
 			)
